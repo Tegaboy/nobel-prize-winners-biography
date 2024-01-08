28fc60541bebd6dbbc18445971e7b14d6d0d23c6
Okandeji John <105325865+Tegaboy@users.noreply.github.com>
2024-01-08 13:18:30 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,183 @@
+(function() {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+  
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+  
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+  
+  const scrollto = (el) => {
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+  
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) {
+        body.classList.remove('mobile-nav-active')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    let count = localStorage.getItem("visitorCount");
+
+    if (count === null) {
+        count = 0;
+    }
+
+    document.getElementById("visitorCount").innerText = count;
+
+    count++;
+
+    localStorage.setItem("visitorCount", count);
+  });
+
+  /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    
+    // Ticker functionality
+    const ticker = document.querySelector('.ticker');
+    setInterval(() => {
+      
+        const now = new Date();
+        ticker.textContent = `Date: ${now.toLocaleDateString()} | Time: ${now.toLocaleTimeString()} | Location: Retrieving...`;
+        
+        navigator.geolocation.getCurrentPosition(position => {
+            ticker.textContent = `Date: ${now.toLocaleDateString()} | Time: ${now.toLocaleTimeString()} | Location: ${position.coords.latitude.toFixed(2)}, ${position.coords.longitude.toFixed(2)}`;
+        });
+    }, 1000);
+  });
+
+  function popups() {
+    document.querySelectorAll('.pics img').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.popup').style.display = 'block';
+            document.querySelector('.popup img').src = image.getAttribute('src');
+        }
+    });
+    
+    document.querySelector('.popup span').addEventListener("click", () => {
+        document.querySelector('.popup').style.display = 'none';
+    })
+  }
+
+  popups()
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
